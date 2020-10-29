e44bd9a37980c4e719cd90519207e8ef55f440b0
Talisson Ferreira <talissonf@gmail.com>
2020-10-29 17:55:48 -0300

    test(deploy): remove requires for pkg fast-xml-parser

--- functions/lib/pagseguro/js-to-xml.js
+++ functions/lib/pagseguro/js-to-xml.js
@@ -1,37 +1,15 @@
-/* const js = require('jstoxml')
+
+// const FastXmlParser = require('fast-xml-parser')
+// const j2xParser = new FastXmlParser.j2xParser()
 
 const jsToXML = data => {
-  const xmlOptions = {
-    attributesFilter: {
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      '\'': '&apos;',
-      '&': '&amp;'
-    },
-    filter: {
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      '\'': '&apos;',
-      '&': '&amp;'
-    }
-  }
-
-  return js.toXML(data, xmlOptions)
-}
-
-module.exports = jsToXML */
-
-const FastXmlParser = require('fast-xml-parser')
-const j2xParser = new FastXmlParser.j2xParser()
-
-const jsToXML = data => {
-  return j2xParser.parse(data)
+  //return j2xParser.parse(data)
+  return {}
 }
 
 const xmlToJson = data => {
-  return FastXmlParser.parse(data)
+  // return FastXmlParser.parse(data)
+  return {}
 }
 
 module.exports = {
